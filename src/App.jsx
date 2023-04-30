370a6a797bed796acf60182d21c3fe3f18699fec
Toma John Musa <tomajohn316@yahoo.com>
2023-04-30 21:59:24 +0100

    Install react router, anchorlinks, icons and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => (
-  <>
-    <Header />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
+import AnchorLink from 'react-anchor-link-smooth-scroll-v2';
+
 const Nav = () => (
-  <>Nav</>
+  <header className="container">
+    <div className="logo">
+      <Link to="/">
+        <img src={logo} alt="Logo" />
+      </Link>
+    </div>
+
+    <nav>
+      <ul>
+        <li>
+          <AnchorLink href="#">About</AnchorLink>
+        </li>
+        <li>
+          <AnchorLink href="#">Reviews</AnchorLink>
+        </li>
+        <li>
+          <AnchorLink href="#">FAQs</AnchorLink>
+        </li>
+        <li>
+          <button className="btn">Get Started</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Nav;
