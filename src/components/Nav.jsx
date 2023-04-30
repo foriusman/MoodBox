ef0061a9db34d286f8f88e08e83d08771a1664c2
Toma John Musa <tomajohn316@yahoo.com>
2023-04-30 22:45:11 +0100

    Add mobile nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,68 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
+import hamb from "../assets/images/hamb.svg";
 import AnchorLink from 'react-anchor-link-smooth-scroll-v2';
+import { BsX } from "react-icons/bs";
+import { useState } from "react";
 
-const Nav = () => (
-  <header className="container">
-    <div className="logo">
-      <Link to="/">
-        <img src={logo} alt="Logo" />
-      </Link>
-    </div>
+const Nav = () => {
+  const [active, setActive] = useState(false);
+  
+  const handleNav = () => {
+    setActive((state) => !state);
+    document.body.classList.toggle('hidden');
+  }
 
-    <nav>
-      <ul>
-        <li>
-          <AnchorLink href="#">About</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink href="#">Reviews</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink href="#">FAQs</AnchorLink>
-        </li>
-        <li>
-          <button className="btn">Get Started</button>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+  return (
+    <header className="container">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+      </div>
+
+      <nav className="desktop-nav">
+        <ul>
+          <li>
+            <AnchorLink href="#">About</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href="#">Reviews</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href="#">FAQs</AnchorLink>
+          </li>
+          <li>
+            <button className="btn white-btn clash-display">Get Started</button>
+          </li>
+        </ul>
+      </nav>
+      <button className="hamb" onClick={handleNav}>
+        <img src={hamb} alt="hamburger icon" />
+      </button>
+
+      { active && <nav className="mobile-nav">
+        <button className="close" onClick={handleNav}>
+          <BsX />
+        </button>
+
+        <ul>
+          <li>
+            <Link href="#">About</Link>
+          </li>
+          <li>
+            <Link href="#">Reviews</Link>
+          </li>
+          <li>
+            <Link href="#">FAQs</Link>
+          </li>
+          <li>
+            <button className="btn white-btn clash-display">Get Started</button>
+          </li>
+        </ul>
+      </nav> }
+    </header>
+  );
+};
 
 export default Nav;
